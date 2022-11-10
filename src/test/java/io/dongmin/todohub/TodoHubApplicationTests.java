206dfcdb3277ae4c1adc51fd2587c58a084d0e26
ddmkim94 <ddmkim94@gmail.com>
2022-11-10 19:59:13 +0900

    프로젝트 생성

---
+++ src/test/java/io/dongmin/todohub/TodoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dongmin.todohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
